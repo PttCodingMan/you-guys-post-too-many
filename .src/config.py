e1cd5e77d27470965e57a65e6aabd9a961dc7f49
PttCodingMan <pttcodingman@gmail.com>
2022-09-03 18:18:46 +0800

    Add Gamesale rules.

--- .src/config.py
+++ .src/config.py
@@ -9,8 +9,7 @@ access_token = os.environ['access_token']
 access_token_secret = os.environ['access_token_secret']
 bearer_token = os.environ['bearer_token']
 
-
-version = '0.0.3'
+version = '0.0.4'
 
 board_rules = [
     ('give', [(None, 3, 1)], True, 'https://www.ptt.cc/bbs/give/M.1612495900.A.C32.html'),
@@ -23,23 +22,13 @@ board_rules = [
     ], True, 'https://www.ptt.cc/bbs/Gossiping/M.1637425085.A.07D.html'),
     ('AllTogether', [(None, 1, 7)], True, 'https://www.ptt.cc/bbs/AllTogether/M.1643211430.A.5FB.html'),
     ('Beauty', [(None, 3, 1)], True, 'https://www.ptt.cc/bbs/Beauty/M.1630069980.A.84B.html'),
-    ('nCoV2019', [(None, 4, 1)], False, 'https://www.ptt.cc/bbs/nCoV2019/M.1584500627.A.886.html'),
-    # ('NBA', [
-    #     ('[討論]', 1, 1),
-    #     ('[專欄]', 1, 1),
-    #     ('[情報]', 1, 1),
-    #     ('[新聞]', 1, 1),
-    #     ('[花邊]', 1, 1),
-    # ], False, 'https://www.ptt.cc/bbs/NBA/M.1637555315.A.C34.html'),
-    ('Lifeismoney', [(None, 15, 30)], False, 'https://www.ptt.cc/bbs/Lifeismoney/M.1629830908.A.94F.html'),
-    ('Hsinchu', [
-        ('[新聞]', 3, 7),
-        ('[合購]', 1, 7),
-        ('[廣宣]', 1, 30),
-        ('[代買]', 1, 30),
-        ('模特', 1, 30),
-        ('共乘', 2, 7),
-    ], False, 'https://www.ptt.cc/bbs/Hsinchu/M.1572354348.A.E2D.html'),
+    ('Gamesale', [
+        (None, 2, 1),
+        ('[販售]', 1, 1),
+        ('[徵求]', 1, 1),
+        ('[交換]', 1, 1),
+        ('[贈送]', 1, 1),
+    ], True, 'https://www.ptt.cc/bbs/Gossiping/M.1637425085.A.07D.html')
 ]
 
 post_template = '''---
